d7b283d52f2e7a3421cf5f963c4017bcd474e861
Arsenii Netrebenko <boecrejha@gmail.com>
2021-08-22 19:47:28 +0300

    📦 NEW: Functions for getting data about slots saturation

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Analytics from './components/Analytics/Analytics.jsx'
+
+function App() {
+	return (
+		<div className="App">
+			<Analytics />
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/Analytics/Analytics.jsx
@@ -0,0 +1,85 @@
+import axios from 'axios'
+import React from 'react'
+import { slots } from './Slots'
+export default function Analytics() {
+	const todayDataURL = 'http://www.aideindustries.tk:5000/today'
+	const weekDataURL = 'http://www.aideindustries.tk:5000/week'
+	const averageURL = 'http://www.aideindustries.tk:5000/avrg/'
+
+	async function getHighSaturatedOnions(url) {
+		let dataOfOnionsToday
+		try {
+			const response = await axios.get(url)
+			dataOfOnionsToday = response.data.data
+		} catch (error) {
+			console.error(error)
+		}
+
+		const highSaturatedOnions = await dataOfOnionsToday.filter(onion => {
+			const firstLevelSaturationTrashHold = 150
+			const onionSaturationValid =
+				onion.Saturation !== 0 && onion.Saturation.includes('%')
+			if (onionSaturationValid) {
+				const saturationNumber = +onion.Saturation.split('%').join('')
+				if (saturationNumber > firstLevelSaturationTrashHold) {
+					return onion
+				}
+			}
+		})
+
+		return highSaturatedOnions
+	}
+
+	async function getAverageOnionsSlotSaturation(onion) {
+		const averageOnionSaturation = await axios.get(`${averageURL}${onion}`)
+		return averageOnionSaturation.data
+	}
+
+	async function getSatReport(slotStart, slotEnd) {
+		const todaySaturatedOnions = await getHighSaturatedOnions(todayDataURL)
+		const weekSaturatedOnions = await getHighSaturatedOnions(weekDataURL)
+		console.log(todaySaturatedOnions, weekSaturatedOnions)
+
+		const namesOfTodaysSaturatedOnions = todaySaturatedOnions.filter(onion =>
+			\
+		)
+	}
+	getSatReport(1, 2)
+
+	return (
+		<div>
+			<form action="#">
+				<select name="slotSelector" id="1">
+					{slots.map((slot, id) => (
+						<option value={slot} key={id}>
+							{slot}
+						</option>
+					))}
+				</select>
+				<select name="slotSelector" id="1">
+					{slots.map((slot, id) => (
+						<option value={slot} key={id}>
+							{slot}
+						</option>
+					))}
+				</select>
+				<button
+					onClick={e => {
+						e.preventDefault()
+					}}
+				>
+					Get report
+				</button>
+			</form>
+		</div>
+	)
+}
+
+
+const link = 'http://www.aideindustries.tk:5000/saturationAnalysis/<onion>/<slot>'
+
+const SaturatedOnion = {
+	name: 'KIE',
+	week_comparison: 'D0 vs D7: курьеров +2%, заказов -6%. ',
+	average_saturation: 'Cредняя сатурация в слотах: 12:00 - 13:00: 78% 13:00 - 14:00: 94% 14:00 - 14:30: 108% 14:30 - 15:30: 118%'
+}
