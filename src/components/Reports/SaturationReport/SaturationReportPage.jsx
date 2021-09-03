a8012026ec40ebf0327f81f9e094f560ef1c1a1d
Arsenii Netrebenko <boecrejha@gmail.com>
2021-09-03 19:31:02 +0300

    Fix issue with getting data about saturated onions

--- src/components/Reports/SaturationReport/SaturationReportPage.jsx
+++ src/components/Reports/SaturationReport/SaturationReportPage.jsx
@@ -2,30 +2,31 @@ import axios from 'axios'
 import React, {useEffect, useState} from 'react'
 import {slots} from '../slots'
 import OnionSaturationCard from "./OnionSaturationCard";
-import {logDOM} from "@testing-library/react";
+import Flex from "../../StyledComponents/Flex";
+import Title from "../../StyledComponents/Title";
 
 export default function SaturationReportPage() {
+
+    const ACCESS_TOKEN = '0a4fcce5995c4f7478bd0b891b265a73d0289ba4'
     const stateReprort = [{
-        city: "DNP",
-        difference: "D0 vs D7: курьеров -13%, заказов +35%. ",
-        reason_saturation: "Причина сатурации - прирост заказов и уменьшилось количество активных курьеров в сравнении с прошлой неделей. ",
-        area: "#A2",
-        saturation: [
-            "19:00 - 20:00: 174",
-            "20:00 - 21:00: 181",
-            "21:00 - 22:00: 161",
-            "22:00 - 22:30: 137"
-        ],
-        level_saturation: " #medium"
+        city: "",
+        difference: "",
+        reason_saturation: "",
+        area: "",
+        saturation: ["",],
+        level_saturation: " "
     }]
 
     const todayDataURL = 'http://www.aideindustries.tk:5000/today'
     const weekDataURL = 'http://www.aideindustries.tk:5000/week'
     const averageURL = 'http://www.aideindustries.tk:5000/avrg/'
-    const jsonReportURL = 'http://www.aideindustries.tk:5000/analysis_json/'
+    const jsonReportURL = 'http://www.aideindustries.tk:8000/analysis/'
     const newApiJSONLink = 'http://www.aideindustries.tk:8000/search/'
-    const [startPeriodSlotSelected, setStartPeriodSlotSelected] = useState('00:00')
-    const [endPeriodSlotSelected, setEndPeriodSlotSelected] = useState('02:00')
+
+    const dataFilterURL = 'http://www.aideindustries.tk:8000/data/filter/'
+
+    const [startPeriodSlotSelected, setStartPeriodSlotSelected] = useState('10:00')
+    const [endPeriodSlotSelected, setEndPeriodSlotSelected] = useState('12:00')
     const [saturationPeriodReport, setSaturationPeriodReport] = useState(stateReprort)
     const [sendRequestForReport, setSendRequestForReport] = useState(false)
 
@@ -36,122 +37,109 @@ export default function SaturationReportPage() {
         } else if (name === 'slotEndPeriodSelector') {
             setEndPeriodSlotSelected(e.target.value)
         }
-
     }
 
-    async function getHighSaturatedOnions(url) {
-        let dataOfOnionsToday
+
+    async function getSaturatedOnionsByPeriod(periodStart, periodEnd) {
         try {
-            const response = await axios.get(url)
-            dataOfOnionsToday = response.data.data
+            const response = await axios.get(`${dataFilterURL}?sat=low&start=${periodStart}&end=${periodEnd}&today=yes`,
+                {
+                    headers: {
+                        'Authorization': `Token ${ACCESS_TOKEN}`
+                    }
+                })
+            return response
         } catch (error) {
-            console.error(error)
+            console.log('Error блин :', error)
+            return error
         }
 
-        return await dataOfOnionsToday.filter(onion => {
-            const firstLevelSaturationTrashHold = 150
-
-            const onionSaturationValid =
-                onion.Saturation !== 0 && onion.Saturation.includes('%')
-            if (onionSaturationValid) {
-                const saturationNumber = +onion.Saturation.split('%').join('')
-                if (saturationNumber > firstLevelSaturationTrashHold) {
-                    return onion
-                }
-            }
-        })
     }
 
-    async function getOnionSaturationInfo(onionName, slotStart, slotEnd) {
-        let info
+    async function getOnionSaturationReportObject(onionName, slotStart, slotEnd) {
         try {
-            const response = await axios.get(`${newApiJSONLink}${onionName}/${slotStart}/${slotEnd}?an=yes`)
-            info = await response.data
+            const response = await axios.get(`${jsonReportURL}${onionName}/${slotStart}/${slotEnd}`, {
+                headers: {'Authorization': `Token ${ACCESS_TOKEN}`}
+            })
+            return await JSON.parse(response.data)
         } catch (error) {
             console.log(error)
         }
-        return info
     }
 
 
-    async function getSatReport(slotStart, slotEnd) {
-        let indexofStartSlotsPeriod = slots.indexOf(slotStart)
-        let indexofEndSlotsPeriod = slots.indexOf(slotEnd) === 0 ? 10000 : slots.indexOf(slotEnd)
+    async function getSatReport(slotStart, slotEnd,) {
+        const slotStartHour = slotStart.substr(0, 2)
+        const slotEndHour = slotEnd.substr(0, 2)
 
-        const todaySaturatedOnions = await getHighSaturatedOnions(todayDataURL)
+        const saturatedOnionsFilteredBySlotsPeriod = await getSaturatedOnionsByPeriod(slotStartHour, slotEndHour)
+        console.log(saturatedOnionsFilteredBySlotsPeriod)
+        const allCodesOfSaturatedOnionsAtSelectedPeriod = saturatedOnionsFilteredBySlotsPeriod.data.reduce((accum, onion) => {
 
-        const sturatedOnionsFilteredBySlotsPeriod = todaySaturatedOnions.filter(onion => {
-            const saturatedSlotOfOnion = onion.Slot.split(" - ")
-            const indexOfSaturatedSlotStart = slots.indexOf(saturatedSlotOfOnion[0])
-            const indexOfSaturatedSlotEnd = slots.indexOf(saturatedSlotOfOnion[1]) === 0 ? 1000 : slots.indexOf(saturatedSlotOfOnion[1])
-            const saturatedSlotIsAtSelectedPeriod = indexofStartSlotsPeriod <= indexOfSaturatedSlotStart && indexofEndSlotsPeriod >= indexOfSaturatedSlotEnd
-
-            if (saturatedSlotIsAtSelectedPeriod) {
-                return onion
-            }
-        })
-
-        const codesOfSaturatedOnionsAtSelectedPeriod = sturatedOnionsFilteredBySlotsPeriod.reduce((accum, onion) => {
-            if (!accum.some(obj => obj.City === onion.City)) {
-                accum.push(onion.City)
+            if (!accum.some(obj => obj.city === onion.city)) {
+                accum.push(onion.city)
             }
             return accum
         }, [])
 
-        const uniqueCodesOfSaturatedOnions = await codesOfSaturatedOnionsAtSelectedPeriod.filter((onionCode, index) => codesOfSaturatedOnionsAtSelectedPeriod.indexOf(onionCode) === index)
-        console.log(uniqueCodesOfSaturatedOnions)
+        const uniqueCodesOfSaturatedOnions = await allCodesOfSaturatedOnionsAtSelectedPeriod.filter((onionCode, index) => allCodesOfSaturatedOnionsAtSelectedPeriod.indexOf(onionCode) === index)
 
-
-        const reportArray = await Promise.all( uniqueCodesOfSaturatedOnions.map(async name => {
-            const slotStartHour = slotStart.substr(0, 2)
-            const slotEndHour = slotEnd.substr(0, 2)
+        const reportArray = await Promise.all(uniqueCodesOfSaturatedOnions.map(async name => {
             const onionCode = name.toLowerCase()
-            const onionSaturationReport = await getOnionSaturationInfo(onionCode, slotStartHour, slotEndHour)
-            return JSON.parse(await onionSaturationReport)
+            return await getOnionSaturationReportObject(onionCode, slotStartHour, slotEndHour)
         }, []))
-
-        console.log(reportArray)
 
         return reportArray
     }
 
-    useEffect(async () => {
+    useEffect(() => {
+        async function fetchData() {
             setSaturationPeriodReport(await getSatReport(startPeriodSlotSelected, endPeriodSlotSelected))
-        }, [sendRequestForReport]
-    )
+        }
+
+        fetchData()
+    }, [sendRequestForReport])
+
 
     return (
-        <div>
-            <form action="#">
-                <select name="slotStartPeriodSelector" id="1" value={startPeriodSlotSelected}
-                        onChange={(e) => selectChangeHandler(e)}>
-                    {slots.map((slot, id) => (
-                        <option value={slot} key={id}>
-                            {slot}
-                        </option>
-                    ))}
-                </select>
-                <select name="slotEndPeriodSelector" id="2" value={endPeriodSlotSelected}
-                        onChange={(e) => selectChangeHandler(e)}>
-                    {slots.map((slot, id) => (
-                        <option value={slot} key={id}>
-                            {slot}
-                        </option>
-                    ))}
-                </select>
-                <button
-                    onClick={e => {
-                        getSatReport(startPeriodSlotSelected, endPeriodSlotSelected)
-                        e.preventDefault()
-                        setSendRequestForReport(true)
-                    }}
-                >
-                    Get report
-                </button>
-            </form>
+        <Flex direction={'column'} align={'center'} margin={'4em 0 0 0'}>
+            <Flex>
+                <form action="#">
 
-            <div>{saturationPeriodReport.map((onion, id) => <OnionSaturationCard {...onion} key={id}/>)}</div>
-
-        </div>
+                    <select name="slotStartPeriodSelector" id="1" value={startPeriodSlotSelected}
+                            onChange={(e) => selectChangeHandler(e)}>
+                        {slots.map((slot, id) => (
+                            <option value={slot} key={id}>
+                                {slot}
+                            </option>
+                        ))}
+                    </select>
+                    <select name="slotEndPeriodSelector" id="2" value={endPeriodSlotSelected}
+                            onChange={(e) => selectChangeHandler(e)}>
+                        {slots.map((slot, id) => (
+                            <option value={slot} key={id}>
+                                {slot}
+                            </option>
+                        ))}
+                    </select>
+                    <button
+                        onClick={e => {
+                            e.preventDefault()
+                            setSendRequestForReport(!sendRequestForReport)
+                        }}
+                    >
+                        Get report
+                    </button>
+                </form>
+            </Flex>
+            <Title align={'center'} fWeight={'800'} >{`Апдейт по сатурации с ${startPeriodSlotSelected} по ${endPeriodSlotSelected}`}</Title>
+            <div>
+                {saturationPeriodReport.map((onion, id) => {
+                    if (onion) {
+                        return <OnionSaturationCard {...onion} key={id}/>
+                    }
+                })}
+            </div>
+        </Flex>
     )
 }
