e5d197d1968bf17cf72546a0a8a0dfde25802687
Arsenii Netrebenko <boecrejha@gmail.com>
2021-11-26 22:45:49 +0200

    shulyak removed

---
+++ src/store/helpers/OpsManagersData.ts
@@ -0,0 +1,6 @@
+export enum OpsManagersTelegramNicks {
+    OLHA_FIONOVA = '@Omorfiya',
+    SERGEY_SEREZHENKO = '@sportsmenserega',
+    DMITRIY_TUKAN = '@Дмитрий',
+    ROMAN_PROTSIV = '@protsiv_roman',
+}
