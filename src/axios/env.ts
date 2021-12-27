d5ad309d1d2b3bb9a96b0b13782520a13981a385
Arsenii Netrebenko <boecrejha@gmail.com>
2021-12-27 12:26:03 +0200

    domain update

--- src/axios/env.ts
+++ src/axios/env.ts
@@ -1,4 +1,4 @@
-const REACT_APP_API_URL: string = 'https://aideindustries.ml/'
+const REACT_APP_API_URL: string = 'https://aideindustries.tk/'
 const REACT_APP_API_KEY: string = 'ace68d53680920b040d5e419b048c4d99be6d65f'
 const REACT_APP_ONION_SLOTS_LINK: string =
     'https://beta-admin.glovoapp.com/couriers/schedule/slots/'
