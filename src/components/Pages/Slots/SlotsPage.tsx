5839bd2bbb2933a048b1a578c038e8fa387a4874
Arsenii Netrebenko <boecrejha@gmail.com>
2021-12-29 23:04:46 +0200

    dir refactoring

--- src/components/Pages/Slots/SlotsPage.tsx
+++ src/components/Pages/Slots/SlotsPage.tsx
@@ -3,7 +3,7 @@ import { Flex } from 'components/StyledComponents/Flex'
 import dayjs from 'dayjs'
 import { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
-import { OnionCodes } from '../Reports/onionCodes'
+import { OnionCodes } from '../../../helpers/onionCodes'
 import OnionSlotsCard from './OnionSlotCard'
 
 const todayDate: string = dayjs().format('YYYY-MM-DD')
