39888a9cbce36032e228d242c6356626bad92183
Arsenii Netrebenko <boecrejha@gmail.com>
2022-01-22 02:35:54 +0200

    bigger font size for navlinks

--- src/components/StyledComponents/StyledLink.tsx
+++ src/components/StyledComponents/StyledLink.tsx
@@ -24,7 +24,7 @@ interface PropsSNavLink {
 const SNavLink = styled(NavLink)<PropsSNavLink>`
     color: ${({ color }) => color || 'white'};
     text-decoration: ${({ decoration }) => decoration || 'none'};
-    font-size: ${({ fSize }) => fSize || '1em'};
+    font-size: ${({ fSize }) => fSize || '1.2rem'};
     width: ${({ width }) => width || '100%'};
     height: ${({ height }) => height || '100%'};
     padding: ${({ padding }) => padding || '0'};
