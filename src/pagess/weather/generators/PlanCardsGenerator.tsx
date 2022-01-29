793811fa83e02d99d401ddf42a3e317549251c43
Arsenii Netrebenko <boecrejha@gmail.com>
2022-01-30 01:15:47 +0200

    dir ref

---
+++ src/pagess/weather/generators/PlanCardsGenerator.tsx
@@ -0,0 +1,24 @@
+import { OnionPrecipitationCard } from '../cards/OnionPrecipitationCard'
+import nextId from 'react-id-generator'
+import { Areas } from 'store/helpers/Areas'
+import { IOnionWeatherAnalysis } from 'store/slices/weatherActionPlanSlice'
+
+interface DayPlan {
+    [key: string]: IOnionWeatherAnalysis[]
+}
+
+export const generatePlanCards = (dayPlan: DayPlan): React.ReactElement[][] =>
+    [Areas.kyiv, Areas.mio, Areas.small].map(
+        (area: string): React.ReactElement[] =>
+            dayPlan[area].map(
+                (areaOnionPlan: IOnionWeatherAnalysis): React.ReactElement =>
+                    generatePlanCard(areaOnionPlan)
+            )
+    )
+
+const generatePlanCard = (
+    areaOnionPlan: IOnionWeatherAnalysis
+): React.ReactElement => {
+    console.log('areaOnionPlan: ', areaOnionPlan)
+    return <OnionPrecipitationCard {...areaOnionPlan} key={nextId()} />
+}
