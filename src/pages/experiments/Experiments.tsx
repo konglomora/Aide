937d22b964ee13e5894054cf7899992c7ad78b5f
Arsenii Netrebenko <boecrejha@gmail.com>
2022-02-04 23:20:58 +0200

    new version test

--- src/pages/experiments/Experiments.tsx
+++ src/pages/experiments/Experiments.tsx
@@ -55,7 +55,7 @@ const Experiments = () => {
         <Flex direction="column" width="100%" margin="15em 0 0 15em">
             <Button onClick={sheetLog}>Log to sheet</Button>
             <Button onClick={showAlert}>Show Alert</Button>
-            <TextContent>Version 1.0.0</TextContent>
+            <TextContent>Version 1.0.1</TextContent>
         </Flex>
     )
 }
